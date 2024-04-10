2dcbe7695433e0a9c42bf451d9b22f4bf2769d6d
epicgy12 <ccray@csuchico.edu>
2024-04-10 11:47:28 -0700

    Update PasswordTest.cpp

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -14,9 +14,57 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, single_letter_password)
+TEST(PasswordTest, single_letter_password)  //TEST ( Name of test class, Descriptive name of this test case
 {
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(1, actual);  //Result of this function call should be exactly this
+}
+
+TEST(PasswordTest, two_letter_password)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("yy"))
+}
+
+TEST(PasswordTest, three_letter_password)
+{
+	Password my_password;
+	ASSERT_EQ(3, my_password.count_leading_characters("xxx"))
+}
+
+TEST(PasswordTest, four_letter_password)
+{
+	Password my_password;
+	ASSERT_EQ(4, my_password.count_leading_characters("wwww"))
+}
+
+TEST(PasswordTest, five_letter_password)
+{
+	Password my_password;
+	ASSERT_EQ(5, my_password.count_leading_characters("vvvvv"))
+}
+
+TEST(PasswordTest, five_letter_mixed_password)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("abced"))
+}
+
+TEST(PasswordTest, five_letter_tailing_password)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("baaaa"))
+}
+
+TEST(PasswordTest, long_password)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("dkshblaihsdblfibhadsdljsdbhgilyubfviulhvjkdsnzul;iudhafjasbdilfuhds.fkjnsd.iujbas.diufbliadsubsdkjfb.asdufbilasdufb.,sdakjf.nasdkjufbiusaldbfsaldhbfuyadsbuflaysdh,bfasdkjbfiuladsb;uibgasdliufbyulasdhbfalsdkjfbilsaudbilfuasdhb,fjhb.fkuasbdlifuabsd.ubsdfasydlfiyasdguifa;shduifhsad.jkfh;asodiufh;asuidbf.sdubf"))
+}
+
+TEST(PasswordTest, long_char_password)
+{
+	Password my_password;
+	ASSERT_EQ(300, my_password.count_leading_characters("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
 }
